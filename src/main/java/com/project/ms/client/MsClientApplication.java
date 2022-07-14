948442319c7a278c29fecfca0cde04c1446378da
Andres Llontop <andresllontop01@gmail.com>
2022-07-14 16:25:00 -0500

    Se actualiza pom

--- src/main/java/com/project/ms/client/MsClientApplication.java
+++ src/main/java/com/project/ms/client/MsClientApplication.java
@@ -2,10 +2,12 @@ package com.project.ms.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
 
 @SpringBootApplication
 @EnableReactiveMongoAuditing
+@EnableEurekaClient
 public class MsClientApplication {
 
 	public static void main(String[] args) {
