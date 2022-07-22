893f2678608d70befc7a2915363d6227920e89c7
Andres Llontop <andresllontop01@gmail.com>
2022-07-22 11:02:31 -0500

    Se añade funcionalidad de dockerfile

--- src/main/java/com/project/ms/client/MsClientApplication.java
+++ src/main/java/com/project/ms/client/MsClientApplication.java
@@ -1,17 +1,20 @@
 package com.project.ms.client;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
 
 @SpringBootApplication
 @EnableReactiveMongoAuditing
 @EnableEurekaClient
+@EnableAutoConfiguration(exclude = {MongoAutoConfiguration.class})
 public class MsClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MsClientApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(MsClientApplication.class, args);
+  }
 
 }
--- src/main/java/com/project/ms/client/domain/entities/Client.java
+++ src/main/java/com/project/ms/client/domain/entities/Client.java
@@ -1,46 +1,53 @@
 package com.project.ms.client.domain.entities;
 
 import java.util.Date;
-
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Field;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+/**
+ * Client abstract class
+ * 
+ * @version 1.0.0 2022-07-20, La clase corresponde a la primera versión del
+ *          sistema, la misma no ha sufrido refactorizaciones
+ * @author Andres Llontop
+ * @author Diego Caballero
+ */
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public abstract class Client {
-	
-	@Id
-    private String id;
 
-    @Field("phone_number")
-    private String phoneNumber;
+  @Id
+  private String id;
 
-    @Field("mobile_number")
-    private String mobileNumber;
+  @Field("phone_number")
+  private String phoneNumber;
 
-    @Field("email")
-    private String email;
+  @Field("mobile_number")
+  private String mobileNumber;
 
-    @Field("address")
-    private Address address;
+  @Field("email")
+  private String email;
 
-    @Field("status")
-    private char status;
+  @Field("address")
+  private Address address;
 
-    @Field("created_at")
-    @CreatedDate
-    private Date createdAt;
+  @Field("status")
+  private char status;
 
-    @Field("updated_at")
-    @LastModifiedDate
-    private Date updatedAt;
+  @Field("created_at")
+  @CreatedDate
+  private Date createdAt;
+
+  @Field("updated_at")
+  @LastModifiedDate
+  private Date updatedAt;
 }
